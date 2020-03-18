07e623037ccb5afea672482a6c4a6cb3336d7f30
AleGolovanov <firesport.pro@yandex.ru>
2020-03-19 00:37:55 +0300

    Проверка на спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,10 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-
+    str = str.toUpperCase();
+  if (str.includes("XXX") || str.includes("1XBET")) {
+    return true;
+  } else {
+    return false;
+  }
 }
