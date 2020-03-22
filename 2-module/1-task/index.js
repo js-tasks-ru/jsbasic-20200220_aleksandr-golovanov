5d4a620c18915a052f235cadae2cef133eafad30
AleGolovanov <firesport.pro@yandex.ru>
2020-03-22 11:30:34 +0300

    Функция samSalary

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,6 +3,22 @@
  * @param {Object} salaries - объект зарплат
  * @returns {Number}
  */
-function sumSalary(salaries) {
 
+let salaries = {
+    Jonh: 1000,
+    Ann: 1600,
+    Pete: 1300,
+    month: 'December',
+    currently: 'USD',
+    isPayed: false,
 }
+
+function sumSalary(salaries) {
+    let sum = 0;
+    for (let key in salaries) {
+      if (typeof salaries[key] == 'number') {
+        sum += salaries[key];
+      }
+    }
+    return sum;
+  }
